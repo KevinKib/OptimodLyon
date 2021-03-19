dbfb8bc7ac066d59f2cb464520b405f7fc046fd2
Terbah <dorian.terb@gmail.com>
2021-03-19 14:16:28 +0100

    Commit

--- src/optimodLyon/test/io/XMLLoaderTU.java
+++ src/optimodLyon/test/io/XMLLoaderTU.java
@@ -1,6 +1,18 @@
 package optimodLyon.test.io;
 
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+import optimodLyon.io.*;
+import optimodLyon.model.DeliveryPlan;
 
 public class XMLLoaderTU
 {
+    @Test
+    public void testLoadDeliveryPlan()
+    {
+        DeliveryPlan plan;
+        // Fichier qui n'existe pas
+        System.out.println(new java.io.File("."));
+    }
 }
