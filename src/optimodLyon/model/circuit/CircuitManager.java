8824eec8a1cac5f3ac26102f8ae07d05fed6c859
YolannGauvin <56106849+YolannGauvin@users.noreply.github.com>
2021-03-24 16:58:36 +0100

    updated circuitmanager

--- src/optimodLyon/model/circuit/CircuitManager.java
+++ src/optimodLyon/model/circuit/CircuitManager.java
@@ -1,6 +1,6 @@
 package optimodLyon.model.circuit;
 
-import optimodLyon.model.Segment;
+import optimodLyon.model.*;
 
 import java.util.List;
 
@@ -9,10 +9,40 @@ import java.util.List;
  */
 public class CircuitManager
 {
-
-    public CircuitManager() {
+    public Circuit getGraph() {
+        return ciruit;
     }
 
+    public CityMap getCityMap() {
+        return cityMap;
+    }
+
+    private Circuit ciruit;
+    private CityMap cityMap;
+
+    public CircuitManager(Circuit graph, CityMap cityMap) {
+        this.ciruit = graph;
+        this.cityMap = cityMap;
+    }
+
+    public void addRequest(Request request, int beforePickup, int beforeDelivery){
+        Delivery delivery = request.getDelivery();
+        Pickup pickup = request.getPickup();
+
+        List<Waypoint> waypoints = this.ciruit.getwaypoints();
+        waypoints.add(pickup);
+        waypoints.add(delivery);
+
+        List<Edge> edges = this.ciruit.getedges();
+        List<Segment> path = CircuitPlanner1.getShortestPath(this.cityMap, pickup, delivery);
+        int distance = this.getDistance(path);
+        Edge newEdge = new Edge(path, distance, pickup, delivery);
+        edges.add(newEdge);
+        waypoints.add(delivery);
+        waypoints.add(pickup);
+        this.ciruit.setEdges(edges);
+        this.ciruit.setWaypoints(waypoints);
+    }
     public List<Segment> deleteRequest(Request request){
         return null;
     }
