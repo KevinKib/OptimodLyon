045ef1700aba81145e1a7035794d53980f89d49e
Sekanor <sekanor21@gmail.com>
2021-03-24 17:04:54 +0100

    AbstractCircuitPlanner

---
+++ src/optimodLyon/circuitPlanner/AbstractCircuitPlanner.java
@@ -0,0 +1,17 @@
+package optimodLyon.circuitPlanner;
+
+import optimodLyon.model.CityMap;
+import optimodLyon.model.DeliveryPlan;
+import optimodLyon.model.Intersection;
+import optimodLyon.model.Segment;
+
+import java.util.List;
+import java.util.Map;
+
+public abstract class AbstractCircuitPlanner {
+
+    public abstract void searchSolution(CityMap map, DeliveryPlan plan, int cyclists);
+
+    public abstract Map.Entry<List<Segment>, Integer> getShortestPath(CityMap map, Intersection a, Intersection b, int cyclists);
+
+}
