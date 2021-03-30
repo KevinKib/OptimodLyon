37764a6228d64f709186a24c8f294544280dfbc1
Sekanor <sekanor21@gmail.com>
2021-03-30 11:31:43 +0200

    2opt stash

--- src/external/circuitPlanner/AlgorithmeOptimisationLocale.java
+++ src/external/circuitPlanner/AlgorithmeOptimisationLocale.java
@@ -1,14 +1,38 @@
 package external.circuitPlanner;
 
+import optimodLyon.model.Node;
+import optimodLyon.model.Warehouse;
 import optimodLyon.model.circuit.Edge;
 import optimodLyon.model.circuit.Graph;
 
-public class AlgorithmeOptimisationLocale {
+import java.util.ArrayList;
+
+public class AlgorithmeOptimisationLocale extends AlgorithmeVoyageurCommerce{
+
+    public AlgorithmeOptimisationLocale() {
+        super();
+    }
 
     public Graph calculate(Graph g) {
 
         AlgorithmeAleatoire algorithmeAleatoire = new AlgorithmeAleatoire();
         Graph graph = algorithmeAleatoire.calculate(g);
+
+        selectedNodes = new ArrayList<Node>(graph.getNodes());
+
+        boolean amelioration = true;
+
+        while(amelioration) {
+            amelioration = false;
+
+            for (Node i : selectedNodes) {
+                for (Node j : selectedNodes) {
+                    if (i != j && !(i instanceof Warehouse) && !(j instanceof Warehouse)) {
+//                        this.swapOpt(i, j);
+                    }
+                }
+            }
+        }
 
         return null;
     }
